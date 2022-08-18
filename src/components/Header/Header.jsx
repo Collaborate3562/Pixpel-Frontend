f74bd4a1fce071a47f89c5992c760b611df6a15c
Dragon Warrior <95697910+Dragonwarrior1234@users.noreply.github.com>
2022-08-18 21:57:25 +0900

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -79,7 +79,6 @@ const Header = (props) => {
   const [modalIsOpenVerify, setIsOpenVerify] = React.useState(false);
   Modal.defaultStyles.overlay.backgroundColor = 'rgba(0, 0, 0, 0.5)';
 
-<<<<<<< HEAD
   function openModal() {
     setIsOpen(true);
   }
@@ -165,93 +164,6 @@ const Header = (props) => {
                     d="M12.5358 21.4243C12.3468 21.7501 12.0755 22.0205 11.7492 22.2085C11.4228 22.3965 11.0527 22.4955 10.6761 22.4955C10.2995 22.4955 9.92942 22.3965 9.60304 22.2085C9.27666 22.0205 9.0054 21.7501 8.81641 21.4243"
                     stroke="#717A8B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
-=======
-    function openModal() {
-        setIsOpen(true);
-    }
-    function mainHeader () {
-        return (
-            <div className="container-fluid">
-                <div className="header__inner">
-                    <div className="header__logo">
-                        <img src="assets/images/logo.svg" className="logo" alt=""/>
-                    </div>
-                    <Menu/>
-                    <div className="header__group">
-                        <button className="btn" onClick={() => {
-                            typeModal = 'login'
-                            openModal()
-                        }}>Log in
-                        </button>
-                        <button className="btn btn--brand" onClick={() => {
-                            typeModal = 'register'
-                            openModal()
-                        }}>Register
-                        </button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-    function userHeader () {
-        return (
-            <div className="container-fluid">
-                <div className="header__inner">
-                    <div className="header__logo">
-                        <a href="/"><img src="assets/images/logo.svg" className="logo" alt=""/></a>
-                    </div>
-                    <nav className="menu">
-                        <ul className="menu__list">
-                            <li className="menu__item">
-                                <a href="/nft-market" className="menu__link">
-                                    Market
-                                </a>
-                            </li>
-                            <li className="menu__item">
-                                <a href="/stacking" className="menu__link">
-                                    Staking
-                                </a>
-                            </li>
-                            <li className="menu__item">
-                                <a href="/trade" className="menu__link">
-                                    Trade
-                                </a>
-                            </li>
-                            <li className="menu__item">
-                                <a href="/swap-master" className="menu__link">
-                                    Buy crypto
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <a href="" className="header__btn-user">
-                        Player
-                    </a>
-                    <div className="header__group">
-                        <div className="header__user-tools">
-                            <a href="" className="header__link">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M9 22C9.55228 22 10 21.5523 10 21C10 20.4477 9.55228 20 9 20C8.44772 20 8 20.4477 8 21C8 21.5523 8.44772 22 9 22Z"
-                                        stroke="#717A8B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                    <path
-                                        d="M20 22C20.5523 22 21 21.5523 21 21C21 20.4477 20.5523 20 20 20C19.4477 20 19 20.4477 19 21C19 21.5523 19.4477 22 20 22Z"
-                                        stroke="#717A8B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                    <path
-                                        d="M1 1H5L7.68 14.39C7.77144 14.8504 8.02191 15.264 8.38755 15.5583C8.75318 15.8526 9.2107 16.009 9.68 16H19.4C19.8693 16.009 20.3268 15.8526 20.6925 15.5583C21.0581 15.264 21.3086 14.8504 21.4 14.39L23 6H6"
-                                        stroke="#717A8B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
-                            </a>
-                            <a href="" className="header__link">
-                                <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M17.1245 7.44981C17.1245 5.73922 16.445 4.09868 15.2354 2.88911C14.0259 1.67953 12.3853 1 10.6747 1C8.96412 1 7.32359 1.67953 6.11401 2.88911C4.90444 4.09868 4.22491 5.73922 4.22491 7.44981C4.22491 14.9746 1 17.1245 1 17.1245H20.3494C20.3494 17.1245 17.1245 14.9746 17.1245 7.44981Z"
-                                        stroke="#717A8B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                    <path
-                                        d="M12.5358 21.4243C12.3468 21.7501 12.0755 22.0205 11.7492 22.2085C11.4228 22.3965 11.0527 22.4955 10.6761 22.4955C10.2995 22.4955 9.92942 22.3965 9.60304 22.2085C9.27666 22.0205 9.0054 21.7501 8.81641 21.4243"
-                                        stroke="#717A8B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                </svg>
->>>>>>> 350f367110a50aec2a167895a53114cccfa1d0cb
 
               </a>
               <a href="" className="header__link">
@@ -292,7 +204,6 @@ const Header = (props) => {
                 </svg>
               </a>
             </div>
-<<<<<<< HEAD
           </div>
         </div>
       </div>
@@ -334,46 +245,6 @@ const Header = (props) => {
           </a>
           <div className="header__group">
             <div className="header__user-tools">
-=======
-        )
-    }
-    function devHeader () {
-        return (
-            <div className="container-fluid">
-                <div className="header__inner">
-                    <div className="header__logo">
-                        <a href="/"><img src="assets/images/logo.svg" className="logo" alt=""/></a>
-                    </div>
-                    <nav className="menu">
-                        <ul className="menu__list">
-                            <li className="menu__item">
-                                <a href="/buy-crypto" className="menu__link">
-                                    Games
-                                </a>
-                            </li>
-                            <li className="menu__item">
-                                <a href="/nft-market" className="menu__link">
-                                    NFT
-                                </a>
-                            </li>
-                            <li className="menu__item">
-                                <a href="/trade" className="menu__link">
-                                    Token
-                                </a>
-                            </li>
-                            <li className="menu__item">
-                                <a href="/stacking" className="menu__link">
-                                    Staking
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <a href="" className="header__btn-user">
-                        Developer
-                    </a>
-                    <div className="header__group">
-                        <div className="header__user-tools">
->>>>>>> 350f367110a50aec2a167895a53114cccfa1d0cb
 
               <a href="" className="header__link">
                 <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
